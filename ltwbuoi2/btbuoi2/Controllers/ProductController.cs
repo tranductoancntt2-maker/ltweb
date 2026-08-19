1ce5aed6da9f78c3080957008d7a870fd0b0888f
Tdt99999 <tranductoancntt2@gmail.com>
2026-08-19 15:31:21 +0700

    Add btbuoi2

---
+++ ltwbuoi2/btbuoi2/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+﻿using btbuoi2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace btbuoi2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Product 1",
+                    Price = 500000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "/images/product1.webp"
+                },
+
+                new Product
+                {
+                    Id = 2,
+                    Name = "Product 2",
+                    Price = 700000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "/images/product1.webp"
+                },
+
+                new Product
+                {
+                    Id = 3,
+                    Name = "Product 3",
+                    Price = 550000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "/images/product1.webp"
+                },
+
+                new Product
+                {
+                    Id = 4,
+                    Name = "Product 4",
+                    Price = 550000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "/images/product1.webp"
+                }
+            };
+
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var products = new List<Product>
+    {
+        new Product
+        {
+            Id = 1,
+            Name = "Product 1",
+            Price = 500000,
+            CreatedAt = new DateTime(2020, 12, 25),
+            Image = "/images/product1.webp"
+        },
+
+        new Product
+        {
+            Id = 2,
+            Name = "Product 2",
+            Price = 700000,
+            CreatedAt = new DateTime(2020, 12, 25),
+            Image = "/images/product1.webp"
+        },
+
+        new Product
+        {
+            Id = 3,
+            Name = "Product 3",
+            Price = 550000,
+            CreatedAt = new DateTime(2020, 12, 25),
+            Image = "/images/product1.webp"
+        },
+
+        new Product
+        {
+            Id = 4,
+            Name = "Product 4",
+            Price = 550000,
+            CreatedAt = new DateTime(2020, 12, 25),
+            Image = "/images/product1.webp"
+        }
+    };
+
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+    }
+}
---
+++ ltwbuoi2/btbuoi2/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace btbuoi2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Image { get; set; } = "";
+    }
+}
