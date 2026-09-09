1662773d2131edd2830cf926af3641a6fb44525c
Tdt99999 <tranductoancntt2@gmail.com>
2026-09-09 16:01:57 +0700

    add tdtltwbuoi4

---
+++ tdtbuoi4/tdtltwbuoi4/Controllers/tdtRazorCodeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tdtltwbuoi4.Controllers
+{
+    public class tdtRazorCodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
