4be2db31dfdb276f2b8861a49e7d45883e1d0c9b
Tdt99999 <tranductoancntt2@gmail.com>
2026-09-09 16:04:33 +0700

    delete

--- tdtbuoi4/tdtltwbuoi4/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace tdtltwbuoi4.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
