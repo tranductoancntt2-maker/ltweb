d8e273543e9174a75d8ce444fed708b26eac5494
Tdt99999 <tranductoancntt2@gmail.com>
2026-09-09 16:06:47 +0700

    add tdtltwbuoi4

---
+++ tdtbuoi4/tdtltwbuoi4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tdtltwbuoi4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
